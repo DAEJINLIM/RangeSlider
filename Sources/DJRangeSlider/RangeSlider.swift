22a8a1d3c924150004e66654d25d18c6c294f8ab
DAEJINLIM <daejin.limm@gmail.com>
2023-11-08 23:42:04 +0900

    Update RangeSlider.swift

--- Sources/DJRangeSlider/RangeSlider.swift
+++ Sources/DJRangeSlider/RangeSlider.swift
@@ -73,7 +73,7 @@ public class RangeSlider: UIControl {
         }
     }
     
-    override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         addSubView()
         makeConstraints()
